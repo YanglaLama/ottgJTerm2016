fc8dace6cd4a17bfecbe6d861e97c380261157f7
Demo User <demouser@n243-139.mtholyoke.edu>
2016-01-05 15:02:11 -0500

    Able to replace to-do item

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -12,6 +12,14 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def send_keys_to_inputbox(self):
+        inputbox.send_keys(Keys.ENTER)
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has a cool new online to-do app
         # She checks out the homepage
@@ -45,22 +53,23 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates and now the page lists
         # "1. Buy peacock feathers" as an item
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # She can still add more to do items
         # She writes "Use peacock feathers to make fly"
         # (Edith is very methodolical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # The homepage uodates again, and now shows both items on her list
 
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
         # Edith wonders whether the site will remember her list.
         # The site generates a unique URL for her- there is some explanation
-
         # She visits the URL- her to-do list is still there
-
         # She is done!
         self.fail('Finish the app!')
 
