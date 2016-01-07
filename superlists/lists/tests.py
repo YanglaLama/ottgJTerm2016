f984301a60a75a34fbb89f3be5c38744c366d9b2
Demo User <demouser@n242-161.mtholyoke.edu>
2016-01-07 14:47:47 -0500

    Unique URL's for each list

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -45,24 +45,30 @@ class NewListTest(TestCase):
             data = {'item_text': 'A new list item'}
         )
 
-        self.assertRedirects(response, '/lists/the-only-list/')
+        new_list = List.objects.first()
+        self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
 
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list/')
+        new_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (new_list.id,))
         self.assertTemplateUsed(response, 'list.html')
 
-    def test_displays_all_items(self):
-        new_list = List.objects.create()
+    def test_displays_only_items_for_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text = 'itemey 1', list = correct_list)
+        Item.objects.create(text = 'itemey 2', list = correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text = 'itemey 1', list = other_list)
+        Item.objects.create(text = 'itemey 2', list = other_list)
 
-        Item.objects.create(text = 'itemey 1', list = new_list)
-        Item.objects.create(text = 'itemey 2', list = new_list)
-
-        response = self.client.get('/lists/the-only-list/')
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other item 1')
+        self.assertNotContains(response, 'other item 2')
 
 class ItemAndListModelsTest(TestCase):
 
