2da79f5389c5baf8e9609c0935c7849498ef80ed
Demo User <demouser@n242-168.mtholyoke.edu>
2016-01-08 14:48:26 -0500

    Start alyout tests

--- superlists/functional_tests/test.py
+++ superlists/functional_tests/test.py
@@ -21,9 +21,31 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(todo_text)
         inputbox.send_keys(Keys.ENTER)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_layout_and_styling(self):
         # Edith has a cool new online to-do app
+
         # She checks out the homepage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        self.check_input_box_is_centered()
+
+        # She starts a new list and sees the box is centered
+        self.enter_a_new_item('testing')
+        self.check_input_box_is_centered()
+
+    def check_input_box_is_centered(self):
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta = 5
+        )
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
         self.browser.get(self.live_server_url)
 
         # She notices page title and header mention to-do lists
